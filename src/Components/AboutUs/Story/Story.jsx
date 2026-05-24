d78d922e49b509d7aee3c07db518853284f5987b
Maneeshaprabha <72514560+Maneeshaprabha@users.noreply.github.com>
2026-05-24 09:41:02 +0530

    STORY IMAGE BUG

--- src/Components/AboutUs/Story/Story.jsx
+++ src/Components/AboutUs/Story/Story.jsx
@@ -3,6 +3,11 @@ import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { ArrowUpRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import innovationImage from "../../../assets/innovation.webp";
+import expertImage from "../../../assets/expert.webp";
+import technologyImage from "../../../assets/technology.webp";
+import growingImage from "../../../assets/growing.webp";
+import futureImage from "../../../assets/future.webp";
 
 export default function WhoWeAreModern() {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -18,7 +23,7 @@ export default function WhoWeAreModern() {
       topic:"We are a team focused on building modern digital and AI-powered solutions.",
       tag1: "Innovation",
       tag2: "AI",
-  
+      image: innovationImage,
       color: "bg-gray-100",
     },
     {
@@ -27,7 +32,7 @@ export default function WhoWeAreModern() {
       topic:"We specialize in AI, software development, mobile apps, and UI/UX design.",
       tag1: "Technology",
       tag2: "Development",
-   
+      image: expertImage,
       color: "bg-[#F4F8FC]",
     },
     {
@@ -36,7 +41,7 @@ export default function WhoWeAreModern() {
       topic:"We turn complex business challenges into simple, smart solutions.",
       tag1: "Solutions",
       tag2: "Strategy",
-
+      image: technologyImage,
       color: "bg-[#29AAE3]",
     },
     {
@@ -45,7 +50,7 @@ export default function WhoWeAreModern() {
       topic:"We help businesses scale with efficient and intelligent technology.",
       tag1: "Scalability",
       tag2: "Growth",
-
+      image: growingImage,
       color: "bg-[#0D4DB1]",
     },
     {
@@ -54,7 +59,7 @@ export default function WhoWeAreModern() {
       topic:"We create scalable systems that grow and adapt with your business.",
       tag1: "Future-ready",
       tag2: "Systems",
-   
+      image: futureImage,
       color: "bg-[#1A1A1A]",
     },
   ];
